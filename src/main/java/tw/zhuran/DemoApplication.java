01bea2c0f0a4f8a63cc63d0605e7b5c6c882af2f
Zhu Ran <zhuran94@163.com>
2017-07-31 05:26:21 +0800

    open all cors origins

--- src/main/java/tw/zhuran/DemoApplication.java
+++ src/main/java/tw/zhuran/DemoApplication.java
@@ -19,7 +19,7 @@ public class DemoApplication {
 		return new WebMvcConfigurerAdapter() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**/*").allowedOrigins("http://localhost:8080");
+				registry.addMapping("/**/*");
 			}
 		};
 	}
